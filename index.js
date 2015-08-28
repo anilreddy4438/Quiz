021f0a750a74ff7f8644be262ea3b4619b9560fb
anilreddy4438 <anilkumar190293@gmail.com>
2015-08-28 12:26:48 +0530

    implemented previous button and next button hiding and showing

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var checkbox_answer=[];
 var result=0;
 var s = null;
 var clicks = 1;
-var prev_click = 10;
 
 var array_quiz=[
 
@@ -41,6 +40,9 @@ var array_quiz=[
 function qusetion_quiz(i){
 
     if (i<10 ) {
+        if(i==1){
+            $('.panel-footer').append(' <button type="button" class="btn btn-info prev_pos" onclick="prevClick()">PREVIOUS</button>')
+        }
         $('.panel-body').append('<div class="quiz_text"><div id="quiz_question"> <b>Question: </b> ' + array_quiz[i].question + '</div>' +
             '<div id="quiz_option1"> <input type="radio" name="check" value="A" class="checkbox_check" > ' + '<b>A :</b>' + array_quiz[i].option.A + '</div>' +
             '<div id="quiz_option2"> <input type="radio" name="check" value="B" class="checkbox_check"> ' + '<b>B :</b>' + array_quiz[i].option.B + '</div>' +
@@ -49,6 +51,14 @@ function qusetion_quiz(i){
 
     }
     else{
+        $('.next_pos').hide()
+        $('.prev_pos').hide()
+        for(var g=0;g<=answer_array.length;g++){
+            if(answer_array[g]==checkbox_answer[g]){
+                result+=1
+            }
+        }
+
         $('.panel-body').append('<div><h3> YOUR SCORE : '+result +'</h3></div>')
     }
 
@@ -57,11 +67,10 @@ function qusetion_quiz(i){
     });
 }
 
-
-
     function question_quiz1(j) {
 
         if (j >= 0) {
+
 
         $('.panel-body').append('<div class="quiz_text"><div id="quiz_question"> <b>Question: </b> ' + array_quiz[j].question + '</div>' +
             '<div id="quiz_option1"> <input type="radio" name="check" value="A" class="checkbox_check" > ' + '<b>A :</b>' + array_quiz[j].option.A + '</div>' +
@@ -71,7 +80,8 @@ function qusetion_quiz(i){
 
     }
 else{
-            $('.panel-body').append('<div style=" text-align: center;"><h2> NOTHING IS HERE</h2></div>')
+
+            $('.prev_pos').hide()
     }
         $("input:radio[name='check']").click(function() {
             s=($('input:radio[name=check   ]').filter(":checked").val());
@@ -81,12 +91,14 @@ $.each(array_quiz, function () {
     answer_array.push(this.Answer)
 });
 function clickME() {
+
+    //$('.prev_pos').show()
     $('h1').hide()
     $('h2').hide()
      i = clicks-1;
                 if(clicks!=1){
                     if(s != null) {
-                        checkbox_answer.push(s)
+                        checkbox_answer.splice(i-1,1,s)
                         s = null;
                         var l =  checkbox_answer.length-1;
 
@@ -99,19 +111,30 @@ function clickME() {
                         qusetion_quiz(i)
                     }
                 };
-                if (clicks == 1 ||i == -1) {
+                if (clicks == 1) {
                     clicks += 1;
                     $('.quiz_text').hide()
                     qusetion_quiz(i)
                 }
-
-
+    if(i == -1 || i== -2){
+        clicks += 1;
+    }
 }
 function prevClick(){
     $('h1').hide()
         clicks -= 1;
     j = clicks-1;
+    if(j==0){
+        $('.prev_pos').hide()
+    }
         $('.quiz_text').hide()
     question_quiz1(j);
-    checkbox_answer.splice(j+1,1,s)
-}
+    if(s != null) {
+        checkbox_answer.splice(j+1,1,s)
+        s = null;
+        var l =  checkbox_answer.length-1;
+
+        if ( checkbox_answer[l] == answer_array[l] ) {
+            alert('the answer is correct')
+        }
+}}
